77ca94f51f6cccd86f9fa60882146d7707373ac7
Himanshi Mathur <himanshi18037@iiitd.ac.in>
2021-05-05 13:25:37 +0530

    Integrating Summary in UI

--- Website_UI/app.py
+++ Website_UI/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request
 import summary
-import random
 
 app = Flask(__name__)
 
@@ -11,7 +10,6 @@ def home():
 
 @app.route("/retrieval", methods = ['GET', 'POST'])
 def retrieval():
-	print("page fetched!!!")
 	return render_template("retrieval_page.html")
 
 @app.route("/summarise", methods = ['GET', 'POST'])
@@ -21,10 +19,8 @@ def summarise():
 
 	if request.method == "POST":
 		input_text = request.form.get("input_patent")
-		# print(input_text)
 		if input_text != "":
 			output_text = summary.get_summary(input_text)
-			# output_text = "abcd" + str(random.randint(1, 100))
 			return render_template("summary_page.html", show_hidden = True, input_text = input_text, output_text = output_text)
 
 	return render_template("summary_page.html", show_hidden = False)
